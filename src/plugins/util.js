55479b14e5c611be032943f5c07f961b66941b5a
DDam40 <craftyworks@gmail.com>
2020-04-23 21:50:32 +0900

    Update util.js

--- src/plugins/util.js
+++ src/plugins/util.js
@@ -1,17 +1,26 @@
-import { mapState, mapMutations } from 'vuex'
-
 export default {
   install (Vue) {
     const mixin = {
       data () {
         return {
           steps: [
+            { c: '#F63538', v: -5 },
+            { c: '#F63538', v: -3 },
+            { c: '#BF4045', v: -2 },
+            { c: '#8B444E', v: -1 },
+            { c: '#414554', v: 0 },
+            { c: '#35764E', v: 1 },
+            { c: '#2F9E4F', v: 2 },
+            { c: '#30CC5A', v: 3 },
+            { c: '#30CC5A', v: 5 }
+          ],
+          steps_red_blue: [
             { c: '#1939B7', v: -5 },
             { c: '#254EDB', v: -3 },
             { c: '#3366FF', v: -1 },
             { c: '#6690FF', v: -0.1 },
-            // { c: '#868e96', v: 0 },
-            { c: '#414554', v: 0 },
+            { c: '#bfc5c9', v: 0 },
+            // { c: '#414554', v: 0 },
             { c: '#FF503D', v: 0.1 },
             { c: '#f22613', v: 1 },
             { c: '#D00E0D', v: 3 },
@@ -21,12 +30,9 @@ export default {
       },
       computed: {
         stepsFilter () {
-          return this.steps.filter(e => e.v !== 0.1 && e.v !== -0.1).reverse()
-        },
-        ...mapState(['mouse', 'mobileDevice'])
-      },
-      methods: {
-        ...mapMutations(['mutate'])
+          // return this.steps_red_blue.filter(e => e.v !== 0.1 && e.v !== -0.1).reverse()
+          return this.steps.filter(e => e.v < 5 && e.v > -5).reverse()
+        }
       }
     }
     Vue.mixin(mixin)
