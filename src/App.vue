5f606ed48977c332aff27648107471761cb3b8dc
DDam40 <craftyworks@gmail.com>
2020-04-23 21:49:39 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
   <div id="app">
     <stock-screen></stock-screen>
     <app-footer></app-footer>
-    <stock-modal></stock-modal>
   </div>
 </template>
 
 <script>
 import AppFooter from './components/AppFooter'
-import StockModal from './components/StockModal'
 import StockScreen from './components/StockScreen'
 
 export default {
   name: 'App',
   components: {
-    StockScreen, StockModal, AppFooter
+    StockScreen, AppFooter
+  },
+  methods: {
+    onSize () {
+      this.mutate({ name: 'window', value: { width: window.innerWidth, height: window.innerHeight } })
+      this.mutate({ name: 'mobileDevice', value: window.innerWidth <= this.CONST.MOBILE_WIDTH })
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this._.debounce(this.onSize, 300))
+    this.onSize()
   }
 }
 </script>
@@ -23,6 +31,7 @@ export default {
   @import "assets/app.css";
   @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
   @import url('https://fonts.googleapis.com/css?family=Lato:400,700,900');
+
   #app {
     font-family: Lato, "Segoe UI", sans-serif;
     -webkit-font-smoothing: antialiased;
