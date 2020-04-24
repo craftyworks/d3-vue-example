27d9a38d3531a571dd45a498d3123607cfdd4f67
DDam40 <craftyworks@gmail.com>
2020-04-24 19:03:29 +0900

    Update d3.js

--- src/plugins/d3.js
+++ src/plugins/d3.js
@@ -1,8 +1,8 @@
-import { min, mean, max, select, nest, scaleLinear, format } from 'd3'
+import { min, mean, max, extent, select, nest, range, scaleLinear, format, line, curveBasis } from 'd3'
 
 export default {
   install (Vue) {
-    const d3 = { min: min, mean: mean, max: max, select: select, nest: nest, scaleLinear: scaleLinear, format: format }
+    const d3 = Object.assign({}, { min, mean, max, extent, select, nest, range, scaleLinear, format, line, curveBasis })
     Vue.set(Vue.prototype, 'd3', d3)
   }
 }
