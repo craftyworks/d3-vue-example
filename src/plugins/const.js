baac34456ea086621a4fb91d74595074637c5b77
DDam40 <craftyworks@gmail.com>
2020-04-24 19:03:47 +0900

    Update const.js

--- src/plugins/const.js
+++ src/plugins/const.js
@@ -1,7 +1,9 @@
 export default {
   install (Vue) {
     const constants = Object.freeze({
-      MOBILE_WIDTH: 480
+      MOBILE_WIDTH: 480,
+      MIN_WIDTH: 800,
+      MIN_HEIGHT: 600
     })
     Vue.prototype.CONST = constants
   }
